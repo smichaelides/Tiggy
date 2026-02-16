defa20e2b6cd4996d79b314f5e2be1329bd68d08
Sofia Michaelides <smichaelides@princeton.edu>
2026-02-15 23:53:24 -0500

    frontend google auth

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,13 @@ import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+// Get Google Client ID from environment variable or use default
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 
+  "280285444479-5f2hkpe9iabj5alrbrvdkd4g9lcqdlph.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-  <GoogleOAuthProvider clientId="280285444479-5f2hkpe9iabj5alrbrvdkd4g9lcqdlph.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={googleClientId}>
     <App />
   </GoogleOAuthProvider>
   // </StrictMode>,
